46194108d367d111d4620e7deee3deefe9266b9d
Andrea Wyss <andreawyss@users.noreply.github.com>
2020-10-12 08:20:14 -0700

    Updates

---
+++ precache-manifest.401405c0818773057e52902eb0f20781.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d9e4c5ce734b9d15120a446f4617f97",
+    "url": "/redux-libs-demo-app/index.html"
+  },
+  {
+    "revision": "7e8ad214303297ddf013",
+    "url": "/redux-libs-demo-app/static/css/main.88824ec2.chunk.css"
+  },
+  {
+    "revision": "12af1f8adb7c972ed4ac",
+    "url": "/redux-libs-demo-app/static/js/4.3649e74c.chunk.js"
+  },
+  {
+    "revision": "e0447d07c581cfec01b69ae89d5eb5e8",
+    "url": "/redux-libs-demo-app/static/js/4.3649e74c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0904b57d74a663ab1705",
+    "url": "/redux-libs-demo-app/static/js/5.9ae830b1.chunk.js"
+  },
+  {
+    "revision": "09297db70e9e409a7757",
+    "url": "/redux-libs-demo-app/static/js/ContactsQrs.f9cbb46f.chunk.js"
+  },
+  {
+    "revision": "3c2bf86a81d8e3bc5d21",
+    "url": "/redux-libs-demo-app/static/js/TodosSfr.bab14639.chunk.js"
+  },
+  {
+    "revision": "7e8ad214303297ddf013",
+    "url": "/redux-libs-demo-app/static/js/main.96d7a8f1.chunk.js"
+  },
+  {
+    "revision": "d3e25bba5c08200f9ba2",
+    "url": "/redux-libs-demo-app/static/js/runtime-main.01d779ac.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b31658d93c2a8dc5813646e460614a20.js"
+  "/redux-libs-demo-app/precache-manifest.401405c0818773057e52902eb0f20781.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/redux-libs-demo-app/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
